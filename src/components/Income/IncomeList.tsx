4767e21c5bf445407f39478b6cc98f0df973fe8a
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-04-01 09:00:28 -0300

    Income List Section

---
+++ src/components/Income/IncomeList.tsx
@@ -0,0 +1,47 @@
+import moment from "moment";
+import TransactionInfoCard from "../Cards/TransactionInfoCard";
+import { LuDownload } from "react-icons/lu";
+
+import { TransactionsTypes } from "../../types";
+
+export type Props = {
+  transactions: TransactionsTypes[];
+  onDelete: (id: number) => void;
+  onDownload: () => void;
+};
+
+const IncomeList = ({ transactions, onDelete, onDownload }: Props) => {
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">
+          Income Sources
+        </h5>
+
+        <button 
+          className="card-btn"
+          onClick={onDownload}
+        >
+          <LuDownload className="text-base" />
+          Download
+        </button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {transactions?.map((income) => (
+          <TransactionInfoCard 
+            key={`income_${income.id}`}
+            title={income.source || ''}
+            icon={income.icon}
+            date={moment(income.date).format('Do MMM YYYY')}
+            amount={income.amount}
+            type="income"
+            onDelete={() => onDelete(income.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default IncomeList;
--- src/pages/Dashboard/Income.tsx
+++ src/pages/Dashboard/Income.tsx
@@ -5,6 +5,8 @@ import DashboardLayout from "../../components/layouts/DashboardLayout"
 import IncomeOverview from "../../components/Income/IncomeOverview";
 import Modal from "../../components/Modal";
 import AddIncomeForm, { IncomeTypes } from "../../components/Income/AddIncomeForm";
+import IncomeList from "../../components/Income/IncomeList";
+import DeleteAlert from "../../components/DeleteAlert";
 
 import { API_PATHS } from "../../utils/apiPaths";
 import axiosInstance from "../../utils/axiosInstance";
@@ -16,7 +18,7 @@ const Income = () => {
   const [ incomeData, setIncomeData ] = useState<TransactionsTypes[]>([]);
   const [ loading, setLoading ] = useState<boolean>(false);
   const [ openAddIncomeModal, setOpenAddIncomeModal ] = useState<boolean>(false);
-  const [ openDeleteAlert, setOpenDeleteAlert ] = useState<{ show: boolean, data: TransactionsTypes | null }>({
+  const [ openDeleteAlert, setOpenDeleteAlert ] = useState<{ show: boolean, data: number | null }>({
     show: false,
     data: null,
   });
@@ -107,6 +109,17 @@ const Income = () => {
               onAddIncome={() => setOpenAddIncomeModal(true)}
             />
           </div>
+
+          <IncomeList 
+            transactions={incomeData}
+            onDelete={(id) => {
+              setOpenDeleteAlert({
+                show: true,
+                data: id,
+              });
+            }}
+            onDownload={handleDownloadIncomeDetails}
+          />
         </div>
 
         <Modal
@@ -116,6 +129,17 @@ const Income = () => {
         >
           <AddIncomeForm 
             onAddIncome={handleAddIncome}
+          />
+        </Modal>
+
+        <Modal
+          isOpen={openDeleteAlert.show}
+          onClose={() => setOpenDeleteAlert({ show: false, data: null })}
+          title="Delete Income"
+        >
+          <DeleteAlert 
+            content="Are you sure you want to delete this in income detail?"
+            onDelete={() => deleteIncome(openDeleteAlert.data!)}
           />
         </Modal>
       </div>
